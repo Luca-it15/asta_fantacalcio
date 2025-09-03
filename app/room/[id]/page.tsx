7ea85d751aa244c459c2668c1425ab2d1faecd61
Luca Chiocchetti <luca.chiocchetti@emeal.nttdata.com>
2025-09-03 14:34:41 +0200

    removed unsed file

--- app/room/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import Lobby from '@/components/Lobby';
-
-export default function Room({ params }: { params: { id: string } }) {
-  const username = typeof window !== 'undefined' ? localStorage.getItem('username') : '';
-  return <Lobby username={username || ''} room={params.id} />;
-}
